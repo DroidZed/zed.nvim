ac802ec6d130e5e0f19256749594d1cefe4efa42
DroidZed <41507665+DroidZed@users.noreply.github.com>
2025-09-01 10:51:13 +0100

    feat: replaced neotree with oil

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,9 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  require 'custom.plugins.markerview',
-  require 'custom.plugins.cord',
-  require 'custom.plugins.spellcheck',
-  require 'custom.plugins.transparent',
-}
+return {}
